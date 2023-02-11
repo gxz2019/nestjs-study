cdf9d6b9d33042fd7eb6583ca16f158b7d0122e0
xiangzhe <xiangzhe@xsky.com>
2023-02-11 23:35:02 +0800

    init
    Signed-off-by: xiangzhe <xiangzhe@xsky.com>

---
+++ fast-apiGateway/src/doc.ts
@@ -0,0 +1,20 @@
+/* eslint-disable @typescript-eslint/ban-ts-comment */
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
+//@ts-ignore
+import * as packageConfig from '../package.json';
+
+
+const generateDocument = (app) => {
+  const options = 
+    new DocumentBuilder()
+    .setTitle(packageConfig.name)
+    .setDescription(packageConfig.description)
+    .setVersion(packageConfig.version)
+    .build();
+  const doc = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('/api/doc', app, doc)
+};
+
+export {
+  generateDocument
+}
