97dd26cd656724b5a6559af9c1600b3828d38722
xiangzhe <xiangzhe@xsky.com>
2023-02-18 21:33:18 +0800

    add mongodb
    Signed-off-by: xiangzhe <xiangzhe@xsky.com>

---
+++ fast-apiGateway/src/common/database/database.providers.ts
@@ -0,0 +1,33 @@
+import { getConfig } from 'src/utils';
+import { DataSource, DataSourceOptions } from 'typeorm';
+import { User } from 'src/user/user.mongo.entity';
+
+//设置数据库类型
+const databaseType: DataSourceOptions['type'] = 'mongodb';
+
+const { MONGODB_CONFIG } = getConfig();
+
+const MONGODB_DATABASE_CONFIG = {
+  ...MONGODB_CONFIG,
+  type: databaseType,
+  entities: [User]
+};
+
+const MONGODB_DATA_SOURCE = new DataSource(MONGODB_DATABASE_CONFIG);
+
+//数据库注入
+const DatabaseProviders = [
+  {
+    provide: 'MONGODB_DATA_SOURCE',
+    useFactory: async () => {
+      await MONGODB_DATA_SOURCE.initialize();
+      console.log('("Connection initialized with database...");')
+      console.log(__dirname + '/../**/*.entity.{js}')
+      return MONGODB_DATA_SOURCE;
+    }
+  }
+];
+
+export {
+  DatabaseProviders
+}
